97840b1453e7c0ddeebc465b110ca85bce69524c
James Le <le_j6@denison.edu>
2017-01-06 12:03:31 +0000

    added devise

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'devise', '~> 4.2'
 
 group :development, :test do
   gem 'sqlite3', '1.3.9'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Added as per devise installation instructions
+  config.action_mailer.default_url_options = { host: 'pinterested1994.herokuapp.com' }
 end
