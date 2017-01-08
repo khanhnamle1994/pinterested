af01bd3b3414f45779af369195b6c47b41758ebe
James Le <le_j6@denison.edu>
2017-01-08 07:56:52 +0000

    created pins scaffold editied pin views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :pins
   devise_for :users
   root 'home#index'
   get 'home/about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106115559) do
+ActiveRecord::Schema.define(version: 20170108073535) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
